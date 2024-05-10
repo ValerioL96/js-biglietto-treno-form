1ea7bcfae41b8dc40dedaf8117a3e29b6d6319b0
ValerioL96 <v.lembo1396@gmail.com>
2024-05-10 02:12:08 +0200

    Added new style and edited JavaScript elements

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,27 @@
 
 
-const bottone = document.querySelector('body > input[type="button"]');
-bottone.addEventListener('click', function(){
+const bottone = document.querySelector('button');
+bottone.addEventListener('click', function() {
     let userTravKmEl = document.querySelector('input[type="text"');
     let userAgeEl = document.querySelector('input[type="number"');
 
-     userTravKmEl = Number.parseFloat( prompt('Type the km to travel'), 10);
-     userAgeEl = Number.parseInt( prompt('Type your age'), 10);
+     userTravKmEl = Number.parseFloat('Type the km to travel'), 10;
+     userAgeEl = Number.parseInt('Type your age'), 10;
+
+     console.log(userTravKmEl.value , userAgeEl.value);
 
      const costKmEl = 0.267113;
 
      let discount = 0;
      if(userAgeEl<21){
         discount =24.552;
-     }else if(userAge > 63){
+     }else if(userAgeEl>63){
         discount =37.893;
      }
 
-     let costTicket = costKmEl * userTravKmEl;
-    costTicket = costTicket - ((costTicket/100) * discount);
-    console.log(costTicket.toFixed(2), costTicket)
-})
+
+     let costTicket = costKmEl.value * userTravKmEl.value;
+     costTicket = costTicket - ((costTicket/100) * discount);
+     let hTwo =document.querySelector('h2');
+     hTwo.innerHTML += console.log(costTicket.toFixed(2), costTicket);
+} )
